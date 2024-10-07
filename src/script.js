c059c95c42f07154c6ef74387766db4b5685b58c
avitoras <yamadeypon@gmail.com>
2024-10-08 02:36:24 +0300

    удалил папку

--- src/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.clickable').forEach(function(element) {
-    element.addEventListener('click', function() {
-        const details = element.nextElementSibling; // Получаем следующий элемент (div с классом details)
-        const isExpanded = element.parentElement.getAttribute('data-expanded') === 'true';
-
-        details.style.display = isExpanded ? 'none' : 'block'; // Скрываем или показываем детали
-        element.parentElement.setAttribute('data-expanded', !isExpanded); // Обновляем атрибут
-    });
-});
