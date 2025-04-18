6270bc1001a1a7016ac3c0b3107ce9f16dbf10fd
avitoras <yamadeypon@gmail.com>
2025-04-19 01:54:34 +0300

    пофиксил баг

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('loaded');
-    const themeToggleBtn = document.getElementById('theme-toggle');
-    const body = document.body;
-    const themeStorageKey = 'user-theme'; 
-    function applyTheme(theme) {
-        if (theme === 'dark') {
-            body.classList.add('dark-theme');
-        } else {
-            body.classList.remove('dark-theme');
-        }
-    }
-    const savedTheme = localStorage.getItem(themeStorageKey);
-    let initialTheme;
-    if (savedTheme) {
-        initialTheme = savedTheme; 
-    } else {
-        
-        initialTheme = 'light'; 
-    }
-    applyTheme(initialTheme);
-    updateThemeIcons(initialTheme === 'dark');
-     if (!savedTheme) {
-         localStorage.setItem(themeStorageKey, initialTheme);
-     }
-    themeToggleBtn.addEventListener('click', () => {
-        const isDark = body.classList.contains('dark-theme');
-        const newTheme = isDark ? 'light' : 'dark'; 
-        applyTheme(newTheme); 
-        updateThemeIcons(newTheme === 'dark'); 
-        localStorage.setItem(themeStorageKey, newTheme);
-    });
-
 });
